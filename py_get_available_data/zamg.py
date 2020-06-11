d8583cc3ae18ecc42bf0527074af1f8a2ff8173e
daniel <daniel@localhost.localdomain>
2020-06-11 14:18:12 +0200

    improve logging messages

--- py_get_available_data/zamg.py
+++ py_get_available_data/zamg.py
@@ -28,19 +28,23 @@ def fetch_zamg_data():
         os.mkdir(f"{data_path}")
 
     timezone = pytz.timezone("Europe/Vienna")
-    federal_state = ["burgenland", "kaernten", "niederoesterreich", "oberoesterreich", "salzburg", "steiermark", "tirol", "vorarlberg", "wien"]
+    federal_state = ["burgenland", "kaernten", "niederoesterreich",
+                     "oberoesterreich", "salzburg", "steiermark", "tirol", "vorarlberg", "wien"]
 
     raw_text_time = None
     retry = 0
+    max_retrys = 3
     now_hour = int(datetime.now(timezone).strftime("%H"))
     while now_hour != raw_text_time:
-        if retry <= 3:
+        if retry <= max_retrys:
             file_counter = 0
             for state in federal_state:
                 # Station values and current pressure at station level
-                df_stat, raw_text_time = download_zamg_page_data(data_path, state, "stat")
+                df_stat, raw_text_time = download_zamg_page_data(
+                    data_path, state, "stat")
                 # Station values and current pressure at sea level
-                df_red, raw_text_time = download_zamg_page_data(data_path, state, "red")
+                df_red, raw_text_time = download_zamg_page_data(
+                    data_path, state, "red")
                 df_red = df_red[["station", "ldred"]]
 
                 # Merging of the two dataframes and reorder
@@ -53,25 +57,34 @@ def fetch_zamg_data():
                     file_counter = file_counter + 1
                     timestamp_save = datetime.now().strftime("%Y-%m-%d")
                     csvfile = f"{data_path}/{timestamp_save}_{raw_text_time}_ZAMG_PAGE_{state}.csv"
-                    df.to_csv(csvfile, index=False, quoting=csv.QUOTE_NONNUMERIC)
+                    df.to_csv(csvfile, index=False,
+                              quoting=csv.QUOTE_NONNUMERIC)
+                    logging.info("{:18} | Uhrzeit Text {} | Zeit {}".format(
+                        state, raw_text_time, now_hour))
+                else:
+                    logging.warning("{:18} | Uhrzeit Text {} | Zeit {}".format(
+                        state, raw_text_time, now_hour))
 
-                logging.info("{:18} | Uhrzeit Text {} | Zeit {}".format(state, raw_text_time, now_hour))
                 time.sleep(10)
 
             # Check whether all federal states have been successfully loaded
             if file_counter != 9:
-                logging.error("Not all federal states could be downloaded successfully. The process is repeated. | Retry {} ".format(retry))
+                logging.error(
+                    "Not all federal states could be downloaded successfully. The process is repeated. | Retry {}/{} ".format(retry, max_retrys))
                 retry = retry + 1
                 file_counter = 0
-                # Waiting for next retry
+                logging.info(
+                    "-------------------------------------------- Retry {}/{}. The process is repeated in 600 seconds. --------------------------------------------".format(retry, max_retrys))
                 time.sleep(600)
-                logging.info("Download -------------------------------------------- Retry {} --------------------------------------------".format(retry))
             else:
-                logging.info("---------------------- ZAMG Website -----------------------------------")
+                logging.info(
+                    "All data was downloaded from the Zamg website and saved as CSV files.")
 
         else:
-            logging.error("nicht alle Files downgeloadet | Retryversuch {}".format(retry))
+            logging.error(
+                "The maximum number of retries was reached and not all data could be saved. {}/{}".format(retry, max_retrys))
             sys.exit(1)
+
 
 def download_zamg_page_data(data_path, state, pressure):
     """A function to download the current values from the website of ZAMG."""
@@ -91,9 +104,12 @@ def download_zamg_page_data(data_path, state, pressure):
         raw_text = raw_text.replace('&deg;', '')
         raw_text = raw_text.replace('%', '')
         raw_text = raw_text.replace('Windstille', '')
-        raw_text = raw_text.replace('<small style="font-size:0.85em;">m</small>', '')
-        raw_text = raw_text.replace('<small style="font-size:0.85em;">mm</small>', '')
-        raw_text = raw_text.replace('<small style="font-size:0.85em;">hPa</small>', '')
+        raw_text = raw_text.replace(
+            '<small style="font-size:0.85em;">m</small>', '')
+        raw_text = raw_text.replace(
+            '<small style="font-size:0.85em;">mm</small>', '')
+        raw_text = raw_text.replace(
+            '<small style="font-size:0.85em;">hPa</small>', '')
         raw_text = raw_text.replace(
             '<small style="font-size:0.85em;">Windstille</small>', '')
         # Pressure tendency
@@ -123,7 +139,8 @@ def download_zamg_page_data(data_path, state, pressure):
             f.write(raw_text)
             f.close()
 
-        raw_text_date_begin = re.search('<h1 id="dynPageHead">', raw_text).end()
+        raw_text_date_begin = re.search(
+            '<h1 id="dynPageHead">', raw_text).end()
         raw_text_date_end = re.search("</h1>", raw_text).start()
         raw_text_date = raw_text[raw_text_date_begin:raw_text_date_end]
         raw_text_date = raw_text_date[re.search("-", raw_text_date).end():]
@@ -140,7 +157,8 @@ def download_zamg_page_data(data_path, state, pressure):
         # trim data
         measured_values_begin = re.search(
             '<tr class="dynPageTableLine1"><td class="wert">', raw_text).start()
-        measured_values_end = re.search('Die Messwerte in dieser Liste', raw_text).start()
+        measured_values_end = re.search(
+            'Die Messwerte in dieser Liste', raw_text).start()
         measured_values = raw_text[measured_values_begin: measured_values_end]
 
         with open(tempfile, "w") as f:
@@ -166,7 +184,8 @@ def download_zamg_page_data(data_path, state, pressure):
 
         # Creating the Pandas Dataframe
         ld_name = "ld" + pressure
-        df = pd.DataFrame(text_filter, columns=["station", "alt", "t", "rf", "wg", "wsg", "regen", "sonne", ld_name])
+        df = pd.DataFrame(text_filter, columns=[
+                          "station", "alt", "t", "rf", "wg", "wsg", "regen", "sonne", ld_name])
         df = df.dropna()
 
         # Data manipulation
@@ -187,7 +206,8 @@ def download_zamg_page_data(data_path, state, pressure):
 
         return (df, raw_text_time)
     else:
-        logging.error(f"The request for the URL '{url_page}' returned the status code 404")
+        logging.error(
+            f"The request for the URL '{url_page}' returned the status code 404")
         df = None
         raw_text_time = None
         return(df, raw_text_time)
