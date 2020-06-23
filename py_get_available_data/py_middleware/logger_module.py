18109bd750ab8b73f72d4852237c0e7ef08dd7b4
Daniel Naschberger <daniel@naschi.at>
2020-06-23 18:48:05 +0200

    set logging time to localtime

--- py_get_available_data/py_middleware/logger_module.py
+++ py_get_available_data/py_middleware/logger_module.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 def start_logging(folder, program, docker=True):
     """A function to initialize the logging module"""
-    starttime = datetime.utcnow()
+    starttime = datetime.now()
 
     logging_cfg_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logging.cfg')
     if docker:
@@ -29,8 +29,8 @@ def start_logging(folder, program, docker=True):
 
 def end_logging(starttime):
     """A function to create an end logging message"""
-    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    time_diff = datetime.utcnow() - starttime
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    time_diff = datetime.now() - starttime
     time_diff = time_diff.total_seconds()
 
     msg = 'ENDTIME | {} | TIMEDIFFERENCE | {:.1f} seconds'.format(now, time_diff)
