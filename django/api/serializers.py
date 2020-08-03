2f0d6b05c33907d0cf053702e4c8fe19be41c950
Daniel Naschberger <daniel@naschi.at>
2020-08-03 20:12:55 +0200

    add api view for SpatialMosStep

--- django/api/serializers.py
+++ django/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from predictions.models import SpatialMosRun
+from predictions.models import SpatialMosRun, SpatialMosStep
 
 # Serializers define the API representation.
 class SpatialMosRunSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = SpatialMosRun
         fields = ['parameter', 'anal_date', 'parameter', 'complete']
+
+class SpatialMosStepSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = SpatialMosStep
+        fields = ['spatialmos_run', 'spatialmos_run', 'valid_date', 'step']
