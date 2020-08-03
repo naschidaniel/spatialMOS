66fccb0e3fe10e9c665df7ec9825084a1a4f3ec0
Daniel Naschberger <daniel@naschi.at>
2020-08-03 20:12:55 +0200

    add api view for SpatialMosStep

--- django/api/views.py
+++ django/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from predictions.models import SpatialMosRun
-from .serializers import SpatialMosRunSerializer
+from predictions.models import SpatialMosRun, SpatialMosStep
+from .serializers import SpatialMosRunSerializer, SpatialMosStepSerializer
 
 class SpatialMosRunViewSet(viewsets.ModelViewSet):
     queryset = SpatialMosRun.objects.all()
     serializer_class = SpatialMosRunSerializer
+
+class SpatialMosStepViewSet(viewsets.ModelViewSet):
+    queryset = SpatialMosStep.objects.all()
+    serializer_class = SpatialMosStepSerializer
