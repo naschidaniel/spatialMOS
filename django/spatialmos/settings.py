a4733e79d01adb30aee23679111af7b00dabe6a8
Daniel Naschberger <daniel@naschi.at>
2020-08-03 19:15:58 +0200

    add django-cleanup to project

--- django/spatialmos/settings.py
+++ django/spatialmos/settings.py
@@ -53,6 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # django-cleanup
+    'django_cleanup.apps.CleanupConfig',
+
     # spatialMOS app
     'predictions.apps.PredictionsConfig',
     'pages.apps.PagesConfig',
