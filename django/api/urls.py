7a6c16842d956b042e04e80cadd3440efc77c341
Daniel Naschberger <daniel@naschi.at>
2020-08-03 20:05:40 +0200

    rename url to vorhersagelauf

--- django/api/urls.py
+++ django/api/urls.py
@@ -7,7 +7,7 @@ from .views import SpatialMosRunViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'spatialmosrun', SpatialMosRunViewSet)
+router.register(r'vorhersagelauf', SpatialMosRunViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
