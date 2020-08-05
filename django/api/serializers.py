fd1176db1e878ddb2e289339115e3df8783beef2
Daniel Naschberger <daniel@naschi.at>
2020-08-05 20:16:35 +0200

    get steps of last run in api

--- django/api/serializers.py
+++ django/api/serializers.py
@@ -9,7 +9,11 @@ class SpatialMosRunSerializer(serializers.ModelSerializer):
         model = SpatialMosRun
         fields = ['id', 'parameter', 'anal_date', 'parameter', 'complete']
 
-class SpatialMosStepSerializer(serializers.HyperlinkedModelSerializer):
+class SpatialMosStepSerializer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField(source='spatialmos_run.parameter')
+    anal_date = serializers.DateTimeField(source='spatialmos_run.anal_date', format='%Y-%m-%d', input_formats=None)
+    valid_date = serializers.DateTimeField(format='%Y-%m-%d', input_formats=None)
+
     class Meta:
         model = SpatialMosStep
-        fields = ['spatialmos_run', 'spatialmos_run', 'valid_date', 'step']
+        fields = ['id', 'anal_date', 'valid_date', 'step', 'parameter', 'filename_nwp_mean', 'filename_nwp_spread', 'filename_spatialmos_mean', 'filename_spatialmos_spread']
