d08d68400ba136e798951dd389e6d44b773560db
Daniel Naschberger <daniel@naschi.at>
2020-08-10 20:36:01 +0200

    save figures in jpg

--- py_spatialmos/py_middleware/plot_functions.py
+++ py_spatialmos/py_middleware/plot_functions.py
@@ -11,60 +11,61 @@ import datetime as dt
 def plot_forecast(parameter, m, xx, yy, plotparameter, anal_date, valid_date, step, what):
     """A function to create the GEFS and spatialMOS forecast plots."""
 
-    fig = plt.figure(figsize=(15, 15), dpi=96)
+    fig_dpi=96
+    fig = plt.figure(figsize=(1200/fig_dpi, 1200/fig_dpi), dpi=fig_dpi)
 
     if parameter == "tmp_2m" and what == "samos_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="RdBu_r")
-        plt.title("2m Temperatur SAMOS MEAN [°C]", loc="center")
+        plot_title="2m Temperatur SAMOS MEAN [°C]"
         plt.clim(-40, 40)
     elif parameter == "tmp_2m" and what == "samos_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("2m Temperatur SAMOS SPREAD [°C]", loc="center")
+        plot_title="2m Temperatur SAMOS SPREAD [°C]"
         plt.clim(0, 5)
     elif parameter == "tmp_2m" and what == "nwp_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="RdBu_r")
-        plt.title("2m Temperatur GEFS MEAN [°C]", loc="center")
+        plot_title="2m Temperatur GEFS MEAN [°C]"
         plt.clim(-40, 40)
     elif parameter == "tmp_2m" and what == "nwp_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("2m Temperatur GEFS SPREAD [°C]", loc="center")
+        plot_title="2m Temperatur GEFS SPREAD [°C]"
         plt.clim(0, 5)
     elif parameter == "rh_2m" and what == "samos_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="YlGn")
-        plt.title("2m Relative Luftfeuchte SAMOS MEAN [%]", loc="center")
+        plot_title="2m Relative Luftfeuchte SAMOS MEAN [%]"
         plt.clim(0, 100)
     elif parameter == "rh_2m" and what == "samos_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("2m Relative Luftfeuchte SAMOS SPREAD [%]", loc="center")
+        plot_title="2m Relative Luftfeuchte SAMOS SPREAD [%]"
         plt.clim(0, 5)
     elif parameter == "rh_2m" and what == "nwp_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="YlGn")
-        plt.title("2m Relative Luftfeuchte GEFS MEAN [%]", loc="center")
+        plot_title="2m Relative Luftfeuchte GEFS MEAN [%]"
         plt.clim(0, 100)
     elif parameter == "rh_2m" and what == "nwp_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("2m Relative Luftfeuchte GEFS SPREAD [%]", loc="center")
+        plot_title="2m Relative Luftfeuchte GEFS SPREAD [%]"
         plt.clim(0, 5)
     elif parameter == "wind_10m" and what == "samos_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Purples")
-        plt.title("10m Windgeschwindigkeit SAMOS MEAN [km/h]", loc="center")
+        plot_title="10m Windgeschwindigkeit SAMOS MEAN [km/h]"
         plt.clim(0, 10)
     elif parameter == "wind_10m" and what == "samos_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("10m Windgeschwindigkeit SAMOS SPREAD [km/h]", loc="center")
+        plot_title="10m Windgeschwindigkeit SAMOS SPREAD [km/h]"
         plt.clim(0, 10)
     elif parameter == "wind_10m" and what == "nwp_mean":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Purples")
-        plt.title("10m Windgeschwindigkeit GEFS MEAN [km/h]", loc="center")
+        plot_title="10m Windgeschwindigkeit GEFS MEAN [km/h]"
         plt.clim(0, 10)
     elif parameter == "wind_10m" and what == "nwp_spread":
         m.pcolormesh(xx, yy, plotparameter, shading="flat", latlon=True, cmap="Reds")
-        plt.title("10m Windgeschwindigkeit GEFS SPREAD [km/h]", loc="center")
+        plot_title="10m Windgeschwindigkeit GEFS SPREAD [km/h]"
         plt.clim(0, 10)
 
-
-    plt.title(f"Gültig für {valid_date} | Step +{step}", loc="left")
-    plt.title(f"GEFS Lauf {anal_date}", loc="right")
+    plt.title(plot_title, loc="center", fontsize=13)
+    plt.title(f"Gültig für {valid_date} | Step +{step}", loc="left", fontsize=10)
+    plt.title(f"GEFS Lauf {anal_date}", loc="right", fontsize=10)
     m.colorbar(location="right")
 
     m.readshapefile("./data/get_available_data/gadm/gadm36_AUT_shp/gadm36_AUT_0", "aut")
@@ -85,9 +86,9 @@ def plot_forecast(parameter, m, xx, yy, plotparameter, anal_date, valid_date, st
 
     anal_date_timestamp = dt.datetime.strptime(anal_date, "%Y-%m-%d %H:%M")
     anal_date_str = anal_date_timestamp.strftime("%Y%m%d")
-    filename = "{}_step_{:03d}.png".format(anal_date_str, step)
+    filename = f"{anal_date_str}_step_{step:03d}.jpg"
     path_filename = os.path.join(filepath, filename)
-    fig.savefig(path_filename, bbox_inches="tight")
+    fig.savefig(path_filename, bbox_inches="tight", quality=95, optimize=True, progressive=True)
     plt.close(fig=None)
     return path_filename, filename
 
