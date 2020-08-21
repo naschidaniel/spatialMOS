8397c1bdba78267c883f96ed40b410edc8367c49
Daniel Naschberger <daniel@naschi.at>
2020-08-21 21:45:08 +0200

    add adress and latlon form

---
+++ django/predictions/forms.py
@@ -0,0 +1,20 @@
+"""The forms for spatialMOS"""
+
+from django import forms
+
+
+class addressForm(forms.Form):
+    """The form for address inputs"""
+    COUNTRY_CHOICES = (('AT', 'Österreich'), ('IT', 'Italien'))
+
+    street = forms.CharField(label='Straße', required=False)
+    number = forms.IntegerField(label='Hausnummer', required=False)
+    zipcode = forms.IntegerField(label='PLZ', required=False)
+    city = forms.CharField(label='Ort', required=True)
+    country = forms.ChoiceField(choices=COUNTRY_CHOICES, required=False)
+
+
+class latlonForm(forms.Form):
+    """The form for coordinates inputs"""
+    latitude = forms.FloatField(label='Latitude', required=True)
+    longitude = forms.FloatField(label='Longitude', required=True)
