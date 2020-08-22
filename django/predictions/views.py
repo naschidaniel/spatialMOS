7510162ca8d91b9a4e2972860bad9dc8c4e38310
Daniel Naschberger <daniel@naschi.at>
2020-08-22 20:20:45 +0200

    process photon data

--- django/predictions/views.py
+++ django/predictions/views.py
@@ -15,6 +15,24 @@ def request_url(request_url):
         data = None
     return data
 
+def photon_data(photon_url, query_string):
+    """A Function to precess the API Call to photon.komoto.de"""
+
+    # Photon software is open source and licensed under Apache License, Version 2.0
+    # https://github.com/komoot/photon
+    photon_json = request_url(photon_url)
+    photon_properties = photon_json['features'][0]['properties']
+
+    error = ""
+    spatialmos_api_url = ""
+
+    if photon_properties['state'] in ['Tyrol', 'Trentino-Alto Adige/Südtirol']:
+        photon_coordinates = photon_json['features'][0]['geometry']['coordinates']
+        spatialmos_api_url = f"http://localhost/api/spatialmospoint/last/tmp_2m/{photon_coordinates[1]}/{photon_coordinates[0]}/"
+    else:
+        error = f"Ihre Eingabe '{query_string}' führte zu einem Ergebnis außerhalb von Nord- und Südtirols."
+
+    return photon_properties, spatialmos_api_url, error
 
 # Views
 def addressprediction(request):
@@ -22,36 +40,38 @@ def addressprediction(request):
     photon_url = ""
     photon_properties = dict()
     spatialmos_api_url = ""
-    photon_json = ""
-
+    error = ''
     
     if request.method == 'GET':
         address_form = addressForm(request.GET)
 
         if address_form.is_valid():
             country = address_form.cleaned_data['country']
+            postcode = str(address_form.cleaned_data['postcode'])
             city = address_form.cleaned_data['city']
             street = address_form.cleaned_data['street']
             housenumber = str(address_form.cleaned_data['housenumber'])
-            postcode = str(address_form.cleaned_data['postcode'])
 
             query_list = [country, postcode, city, street, housenumber]
-            query_string = ','.join(query_list)
             
-            # Photon software is open source and licensed under Apache License, Version 2.0
-            # https://github.com/komoot/photon
+            query_list_filtered = []
+            for value in query_list:
+                if value not in ['', 'None']:
+                    query_list_filtered.append(value)
+            
+            query_string = ','.join(query_list_filtered)
+            
             photon_url = f"https://photon.komoot.de/api/?q=?{query_string}&limit=1"
-            photon_json = request_url(photon_url)
-
-
+            photon_properties, spatialmos_api_url, error = photon_data(photon_url, query_string)
     else:
         address_form = addressForm()
 
     context = {
         'address_form': address_form,
-        'photon_properties': photon_json,
+        'photon_properties': photon_properties,
         'query_url': photon_url,
-        'spatialmos_api_url': spatialmos_api_url
+        'spatialmos_api_url': spatialmos_api_url,
+        'error': error
     }
     
     return render(request, 'predictions/addressprediction.html', context)
@@ -63,6 +83,7 @@ def pointprediction(request):
     photon_url = ""
     photon_properties = dict()
     spatialmos_api_url = ""
+    error = ''
 
     if request.method == 'GET':
         latlon_form = latlonForm(request.GET)
@@ -73,15 +94,8 @@ def pointprediction(request):
 
             query_string = f"lon={longitude}&lat={latitude}"
 
-            # Photon software is open source and licensed under Apache License, Version 2.0
-            # https://github.com/komoot/photon
             photon_url = f"https://photon.komoot.de/reverse?{query_string}&limit=1"
-            photon_json = request_url(photon_url)
-
-            photon_properties = photon_json['features'][0]['properties']
-            photon_coordinates = photon_json['features'][0]['geometry']['coordinates']
-            spatialmos_api_url = f"http://localhost/api/spatialmospoint/last/tmp_2m/{photon_coordinates[1]}/{photon_coordinates[0]}/"
-
+            photon_properties, spatialmos_api_url, error = photon_data(photon_url, query_string)
     else:
         latlon_form = latlonForm()
 
@@ -89,7 +103,8 @@ def pointprediction(request):
         'latlon_form': latlon_form,
         'photon_properties': photon_properties,
         'query_url': photon_url,
-        'spatialmos_api_url': spatialmos_api_url
+        'spatialmos_api_url': spatialmos_api_url,
+        'error': error
         }
 
     return render(request, 'predictions/pointprediction.html', context)
@@ -101,5 +116,6 @@ def predictions(request):
 
     context = {
         'spatialmosrun': spatialmosrun,
+        'error': ''
     }
     return render(request, 'predictions/predictions.html', context)
