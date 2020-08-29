f454243261c9e214ce281eccbcccb7aa23c237f0
Daniel Naschberger <daniel@naschi.at>
2020-08-29 20:31:17 +0200

    add debug to content pre processors

--- django/spatialmos/context_processors.py
+++ django/spatialmos/context_processors.py
@@ -4,4 +4,8 @@ from django.conf import settings
 
 def custom_context(request):
     """A function to pass variables that are valid in all templates."""
-    return {'LASTCOMMIT': settings.LASTCOMMIT}
+    return_dict = {
+        'DEBUG': settings.DEBUG, 
+        'LASTCOMMIT': settings.LASTCOMMIT, 
+        }
+    return return_dict
