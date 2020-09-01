de61a606020a5c3e3cc0a8a7c723aec5dab1f990
Daniel Naschberger <daniel@naschi.at>
2020-09-01 20:55:17 +0200

    fix

--- django/static/js/src/components/Predictions.jsx
+++ django/static/js/src/components/Predictions.jsx
@@ -25,7 +25,7 @@ export default class Predictions extends React.Component {
 
   componentDidMount() {
     fetch(
-      "https://cors-anywhere.herokuapp.com/https://moses.tirol/api/spatialmosrun/last/tmp_2m/"
+      "//api/spatialmosrun/last/tmp_2m/"
     )
       .then((res) => res.json())
       .then(
