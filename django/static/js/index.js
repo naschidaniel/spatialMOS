a663db0bd63fa6928e6bcd0b36a5769cbe4be556
Daniel Naschberger <acrodaniel@gmail.com>
2020-09-05 18:03:44 +0200

    Manage bootstrap in webpack (#15)
    * drop style and link tags
    
    * add bootstrap to project
    
    * add bootstrap to index.js

--- django/static/js/index.js
+++ django/static/js/index.js
@@ -1,4 +1,5 @@
 import "./style.css"
+import "bootstrap/dist/css/bootstrap.css";
 
 import LeafletMap from "./src/components/LeafletMap.jsx";
 import Predictions from "./src/components/Predictions.jsx";
