b3e4bad7a20edc7b0f90346c48fac883e3f923f0
Daniel Naschberger <daniel@naschi.at>
2020-09-06 12:25:14 +0200

    replace choices to state choices

--- django/predictions/forms.py
+++ django/predictions/forms.py
@@ -5,12 +5,12 @@ from django import forms
 
 class addressForm(forms.Form):
     """The form for address inputs"""
-    COUNTRY_CHOICES = (('Austria', 'Österreich'), ('Italy', 'Italien'))
+    STATE_CHOICES = (('Tirol', 'Nordtirol'), ('Südtirol', 'Südtirol'))
 
     street = forms.CharField(label='Straße', required=False)
     postcode = forms.IntegerField(label='PLZ', required=False)
     city = forms.CharField(label='Ort', required=True)
-    country = forms.ChoiceField(choices=COUNTRY_CHOICES, required=False)
+    state = forms.ChoiceField(choices=STATE_CHOICES, required=False)
 
     street.widget.attrs.update({'placeholder': 'Straße und Hausnummer'})
     city.widget.attrs.update({'placeholder': 'Ort'})
