a15d396c7524913c8f8830500f2aa43236fdee06
Daniel Naschberger <daniel@naschi.at>
2020-09-06 14:34:33 +0200

    add customtooltip

---
+++ django/static/js/src/components/CustomTooltip.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class CustomTooltip extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static get propTypes() { 
+    return { 
+      active: PropTypes.bool,
+      payload: PropTypes.array,
+    }; 
+  }
+
+  render() {
+    const { active, payload } = this.props
+    if (active) {
+      if (payload.length === 0) return (<div>No CustomTooltip available</div>)
+      else {
+        let apiData = payload[0].payload
+        return (
+          <div className="custom-tooltip">
+            <p className="label">{apiData.tooltip_label}</p>
+            <p>{`Mean: ${apiData.spatialmos_mean} ${apiData.unit}`}</p>
+            <p>{`Spread: ${apiData.spatialmos_spread} ${apiData.unit}`}</p>
+          </div>
+        );
+      }
+    }
+    else return (<div>No CustomTooltip available</div>)
+  }
+}
--- django/static/js/src/components/SimpleLineChart.jsx
+++ django/static/js/src/components/SimpleLineChart.jsx
@@ -38,14 +38,19 @@ export default class SimpleLineChart extends Component {
       .then(
         (result) => {
           for (const i in result) {
-            result[i].label = result[i].valid_date + " " + result[i].valid_time 
+            result[i].tooltip_label =
+              result[i].valid_date + " " + result[i].valid_time;
             result[i].spatialmos_min =
               Number(result[i].spatialmos_mean) -
               Number(result[i].spatialmos_spread);
             result[i].spatialmos_max =
               Number(result[i].spatialmos_mean) +
               Number(result[i].spatialmos_spread);
-            result[i].spatialmos_range = [Number(result[i].spatialmos_max), Number(result[i].spatialmos_min)];
+            result[i].spatialmos_range = [
+              Number(result[i].spatialmos_max),
+              Number(result[i].spatialmos_min),
+            ];
+            result[i].unit = "°C";
           }
           this.setState({
             isLoaded: true,
@@ -62,37 +67,51 @@ export default class SimpleLineChart extends Component {
   }
   render() {
     const { error, isLoaded, steps } = this.state;
-    return (
-      <ResponsiveContainer width="100%" height={400}>
-        <ComposedChart
-          data={steps}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="valid_date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="spatialmos_mean"
-            stroke="#888888"
-            activeDot={{ r: 8 }}
-          />
-          <Area
-            type="monotone"
-            dataKey="spatialmos_range"
-            fill="#8884d8"
-            opacity="0.3"
-          />
-        </ComposedChart>
-      </ResponsiveContainer>
-    );
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return (
+        <div className="text-center" width="100%" height="400px">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <ResponsiveContainer width="100%" height={400}>
+          <ComposedChart
+            data={steps}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="valid_date" />
+            <YAxis unit={steps[0].unit} />
+            <Tooltip content={<CustomTooltip />} />
+            <Legend />
+            <Line
+              name="Mittelwert"
+              type="monotone"
+              dataKey="spatialmos_mean"
+              stroke="#888888"
+              activeDot={{ r: 8 }}
+            />
+            <Area
+              name="Standardabweichung"
+              type="monotone"
+              dataKey="spatialmos_range"
+              fill="#8884d8"
+              opacity="0.3"
+            />
+          </ComposedChart>
+        </ResponsiveContainer>
+      );
+    }
   }
 }
 const wrapper = document.getElementById("simple_line_chart");
