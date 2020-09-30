391d8981338e22e752ec3f1638fc2b2b3f36e9a0
Daniel Naschberger <daniel@naschi.at>
2020-09-30 20:37:49 +0200

    fixed all linting errors

--- django/static/js/index.js
+++ django/static/js/index.js
@@ -2,6 +2,11 @@ import "bootstrap/js/dist/collapse";
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import LeafletMap from "./src/components/LeafletMap.jsx";
-import SimpleLineChart from "./src/components/SimpleLineChart.jsx";
-import Predictions from "./src/components/Predictions.jsx";
+import LeafletMap from "./src/components/LeafletMap";
+import SimpleLineChart from "./src/components/SimpleLineChart";
+import Predictions from "./src/components/Predictions";
+
+
+LeafletMap
+SimpleLineChart
+Predictions
