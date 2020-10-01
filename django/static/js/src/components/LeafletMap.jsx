22823580f9363967d8a33022b28a913bee1894cd
Daniel Naschberger <daniel@naschi.at>
2020-10-01 19:15:01 +0200

    fixed all linting eslint errors

--- django/static/js/src/components/LeafletMap.jsx
+++ django/static/js/src/components/LeafletMap.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import { DataExchangeObject } from "../middleware/DataExchange.jsx";
+import { DataExchangeObject } from "../middleware/DataExchange";
 
 // require style and marker pngs
 import "leaflet/dist/leaflet.css";
@@ -11,36 +11,37 @@ require("leaflet/dist/images/marker-icon-2x.png");
 require("leaflet/dist/images/marker-icon.png");
 require("leaflet/dist/images/marker-shadow.png");
 
-export default class Leafletmap extends Component {
-  constructor(props) {
-    super(props);
-  }
 
-  static get propTypes() {
-    return {
-      data: PropTypes.object,
-    };
-  }
 
-  render() {
-    const {data} = this.props;
-    return (
-      <Map
-        center={[data.lat, data.lon]}
-        zoom={14}
-        style={{ width: "100%", height: "400px" }}
-      >
-        <TileLayer
-          attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[data.lat, data.lon]}>
-          <Popup>{data.display_name}</Popup>
-        </Marker>
-      </Map>
+function LeafletMap(props) {
+  const {data} = props;
+  return (
+    <Map
+      center={[data.lat, data.lon]}
+      zoom={14}
+      style={{ width: "100%", height: "400px" }}
+    >
+      <TileLayer
+        attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[data.lat, data.lon]}>
+        <Popup>{data.display_name}</Popup>
+      </Marker>
+    </Map>
     );
-  }
 }
+
+LeafletMap.propTypes = {
+  data: PropTypes.shape({ lat: PropTypes.number.isRequired, lon: PropTypes.number.isRequired, display_name: PropTypes.string.isRequired}),
+};
+
+LeafletMap.defaultProps = {
+  'data': undefined,
+};
+
 const wrapper = document.getElementById("leaflet_map");
-const data = DataExchangeObject(wrapper?.attributes?.data);
-wrapper ? ReactDOM.render(<Leafletmap data={data} />, wrapper) : false;
+if (wrapper !== null) {
+  const data = DataExchangeObject(wrapper?.attributes?.data);
+  ReactDOM.render(<LeafletMap data={data} />, wrapper)
+}
