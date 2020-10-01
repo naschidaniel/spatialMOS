92b26e05eb0d231a973a7187b3303b4d3fd8b645
Daniel Naschberger <daniel@naschi.at>
2020-10-01 19:35:21 +0200

    fixed errors from eslint

--- django/static/js/src/components/CustomTooltip.jsx
+++ django/static/js/src/components/CustomTooltip.jsx
@@ -1,32 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class CustomTooltip extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export default function CustomTooltip(props) {
+  const {active, payload} = props
+  if (!active) return <div>No CustomTooltip available</div>;
+  
+  const apiData = payload[0].payload;
 
-  static get propTypes() {
-    return {
-      active: PropTypes.bool,
-      payload: PropTypes.array,
-    };
-  }
-
-  render() {
-    const { active, payload } = this.props;
-    if (active) {
-      if (payload.length === 0) return <div>No CustomTooltip available</div>;
-      
-        const apiData = payload[0].payload;
-        return (
-          <div className="custom-tooltip">
-            <p className="label">{apiData.tooltip_label}</p>
-            <p>{`Mean: ${apiData.spatialmos_mean} ${apiData.unit}`}</p>
-            <p>{`Spread: ${apiData.spatialmos_spread} ${apiData.unit}`}</p>
-          </div>
+  if (payload.length === 0) return <div>No CustomTooltip available</div>;
+  
+  return (
+    <div className="custom-tooltip">
+      <p className="label">{apiData.tooltip_label}</p>
+      <p>{`Mean: ${apiData.spatialmos_mean} ${apiData.unit}`}</p>
+      <p>{`Spread: ${apiData.spatialmos_spread} ${apiData.unit}`}</p>
+    </div>
         );
-      
-    } return <div>No CustomTooltip available</div>;
-  }
+
 }
+
+CustomTooltip.propTypes = {
+    active: PropTypes.bool.isRequired,
+    payload: PropTypes.arrayOf(PropTypes.array()).isRequired,
+  };
