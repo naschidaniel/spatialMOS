156f9a6c8d85c8a77745c25485e38aa98343f3e1
Daniel Naschberger <daniel@naschi.at>
2020-10-04 20:09:48 +0200

    add model for statuschecks

--- django/statusfiles/admin.py
+++ django/statusfiles/admin.py
@@ -1,7 +1,8 @@
 """The models witch are displayed in the Admin Panel"""
 
 from django.contrib import admin
-from .models import StatusFiles
+from .models import StatusChecks, StatusFiles
 
 # Registered models for the Admin panel
+admin.site.register(StatusChecks)
 admin.site.register(StatusFiles)
