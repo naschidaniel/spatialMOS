452c498e19bc7e6a02ccbffb4a5b42c8c9985e73
Daniel Naschberger <daniel@naschi.at>
2021-03-07 20:09:40 +0100

    fix(spatial_writer): write out units in headline

--- py_spatialmos/spatial_writer.py
+++ py_spatialmos/spatial_writer.py
@@ -5,12 +5,16 @@
 import csv
 from typing import Iterable, TextIO
 
+
 class Writer:
     '''Writer for spatialMOS data'''
 
-    def __init__(self, header: Iterable[str], target: TextIO) -> None:
+    def __init__(self, parameters: dict, target: TextIO) -> None:
         self.out = csv.writer(target, delimiter=";")
-        self.out.writerow(header)
+        self.out.writerow([parameters[parameter]['name']
+                           for parameter in parameters])
+        self.out.writerow([parameters[parameter]['unit']
+                           for parameter in parameters])
 
     def append(self, row) -> None:
         '''append a new dataset'''
