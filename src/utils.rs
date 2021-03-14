951f884938308b11e72bbf73e82d84b69a7ed2ff
Daniel Naschberger <daniel@naschi.at>
2021-03-14 21:48:19 +0100

    rust add hasmap

--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,13 @@ pub fn rs_hello_world(name: &str) -> String {
     format!("Hello {}! This message comes from Rust.", name)
 }
 
+
 pub fn rs_convert_datetime() -> String {
     let mut measurements = HashMap::new();
+
+
+    //'2021-01-01T23:00:00CET': {'date': '2021-01-01T23:00:00CET', 'name': '82500WS', 'lat': 46.6156, 'lon': 11.4604, 'alt': 2260, 'LF': 100.5}, 
+    //'2021-01-01T22:00:00CET': {'date': '2021-01-01T22:00:00CET', 'name': '82500WS', 'lat': 46.6156, 'lon': 11.4604, 'alt': 2260, 'LF': 100.5}, 
 
     measurements.insert(
         "date".to_string(),
