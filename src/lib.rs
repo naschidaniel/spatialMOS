2ba5c40c29dd4b94788c3b44bb01fe98ddab25bb
Daniel Naschberger <daniel@naschi.at>
2021-03-18 20:51:01 +0100

    add rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,52 @@
 mod utils;
 
+use chrono::Utc;
 use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList};
 use pyo3::wrap_pyfunction;
+use pyo3::Python;
 
-/// print out a dict
 #[pyfunction]
-fn hello_world(msg: &str) -> PyResult<String> {
-    let greeting = utils::rs_hello_world(msg);
-    Ok(greeting)
+fn convert_measurements(measurements: &PyDict, columns: &PyList) -> PyResult<PyObject> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let utc = Utc::now();
+    let dates = measurements.keys();
+    let measurements_write_lines = PyList::empty(py);
+    match columns.del_item(0) {
+        Err(err) => utils::log_py_error(err),
+        _ => (),
+    }
+    for date in dates {
+        let values: &PyDict = measurements.get_item(&date).unwrap().downcast().unwrap();
+        let date_str = &date
+            .to_string()
+            .replace("CET", "+01:00")
+            .replace("CEST", "+02:00");
+        let row = PyList::empty(py);
+
+        row.append(utils::rs_convert_datetime(&date_str, utc))
+            .map_err(|err| utils::log_py_error(err))
+            .ok();
+        for column in columns {
+            let value = values.get_item(&column);
+            row.append(value)
+                .map_err(|err| utils::log_py_error(err))
+                .ok();
+        }
+        measurements_write_lines
+            .append(row)
+            .map_err(|err| utils::log_py_error(err))
+            .ok();
+    }
+    Ok(measurements_write_lines.into())
 }
 
 /// A python module implemented in Rust for spatialMOS.
 #[pymodule]
 fn spatial_util(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_world, m)?)?;
+    m.add_function(wrap_pyfunction!(convert_measurements, m)?)?;
+
     Ok(())
 }
 
@@ -23,11 +56,14 @@ mod tests {
 
     #[test]
     fn rs_date_hashmap() {
-        assert_eq!("2021-01-01T23:00:00CET", utils::rs_convert_datetime());
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        assert_eq!("Hello Daniel! This message comes from Rust.", utils::rs_hello_world("Daniel"));
+        let utc = Utc::now();
+        assert_eq!(
+            "2021-03-17 23:00:00",
+            utils::rs_convert_datetime("2021-03-18T00:00:00+01:00", utc)
+        );
+        assert_eq!(
+            "2021-03-28 00:00:00",
+            utils::rs_convert_datetime("2021-03-28T02:00:00+02:00", utc)
+        );
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,12 @@
-use std::collections::HashMap;
-use chrono::{DateTime, TimeZone, NaiveDateTime, Utc, Local};
+use chrono::{DateTime, Utc};
+use pyo3::prelude::*;
 
-// print out hello world
-pub fn rs_hello_world(name: &str) -> String {
-    format!("Hello {}! This message comes from Rust.", name)
+pub fn rs_convert_datetime(date: &str, _utc: DateTime<Utc>) -> String {
+    let converted = DateTime::parse_from_rfc3339(&date).unwrap();
+    let utc_converted: DateTime<Utc> = DateTime::from(converted);
+    format!("{}", DateTime::format(&utc_converted, "%Y-%m-%d %H:%M:%S"))
 }
 
-pub fn rs_convert_datetime() -> String {
-    let mut measurements = HashMap::new();
-
-    let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(61, 0), Utc);
-    assert_eq!(Utc.timestamp(61, 0), dt);
-    //'2021-01-01T23:00:00CET': {'date': '2021-01-01T23:00:00CET', 'name': '82500WS', 'lat': 46.6156, 'lon': 11.4604, 'alt': 2260, 'LF': 100.5},
-    //'2021-01-01T22:00:00CET': {'date': '2021-01-01T22:00:00CET', 'name': '82500WS', 'lat': 46.6156, 'lon': 11.4604, 'alt': 2260, 'LF': 100.5},
-
-    let utc = Utc::now();
-    let local = Local::now();
-    let datum = "2021-01-01T23:00:00CET".replace("CET", "+01:00").replace("CEST", "+02:00");
-    measurements.insert("date", &datum);
-
-    let converted = DateTime::parse_from_rfc3339(&datum).unwrap();
-    let utc_converted: DateTime<Utc> = DateTime::from(converted);
-    let utc_converted_str = DateTime::format(&utc_converted, "%Y-%m-%d %H:%M:%S");
-    println!("{:?}", converted);
-    println!("{:?}", utc_converted);
-    println!("{}", utc_converted_str);
-
-    format!("{}", measurements["date"])
+pub fn log_py_error(err: PyErr) {
+    println!("{:?}", err)
 }
