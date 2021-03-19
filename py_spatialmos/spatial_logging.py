19151a2718bf2e080078b78027f02caa3f2fe28d
Daniel Naschberger <acrodaniel@gmail.com>
2021-03-19 07:42:35 +0100

    Using the power of Rust (#33)
    * init(Rust): pylib
    
    * chore(rust): add test
    
    * chore(rust): convert dict
    
    * rust add hasmap
    
    * parse datetime
    
    * add rust
    
    * add rust

--- py_spatialmos/spatial_logging.py
+++ py_spatialmos/spatial_logging.py
@@ -10,6 +10,7 @@ class spatial_logging:
     @classmethod
     def logging_init(cls, logging_file: Path):
         """logging_init the basic settings for logging"""
+        logging_file.parents[0].mkdir(parents=True, exist_ok=True)
         logging.basicConfig(
             format='%(asctime)s - %(levelname)s - %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S',
