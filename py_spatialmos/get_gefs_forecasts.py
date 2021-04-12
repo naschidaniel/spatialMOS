c4ee21ca2ee213970e00a9bf9527c665ce808f60
Daniel Naschberger <daniel@naschi.at>
2021-04-12 20:40:06 +0200

    chore(get_gefs_forecasts): prettier

--- py_spatialmos/get_gefs_forecasts.py
+++ py_spatialmos/get_gefs_forecasts.py
@@ -14,8 +14,10 @@ from datetime import datetime
 import subprocess as sub
 import requests
 
+
 class IdxEntry(object):
     '''A small helper class to handle index entries.'''
+
     def __init__(self, args):
         '''IdxEntry(args)
 
@@ -27,9 +29,9 @@ class IdxEntry(object):
             list with three entries (bytes start, param name, param level)
         '''
         self._byte_start = int(args[0])
-        self._var        = str(args[1])
-        self._lev        = str(args[2])
-        self._byte_end   = False
+        self._var = str(args[1])
+        self._lev = str(args[2])
+        self._byte_end = False
 
     def add_end_byte(self, x):
         '''add_end_byte(x)
@@ -73,7 +75,7 @@ class IdxEntry(object):
         except Exception as ex:
             raise Exception(ex)
 
-        return '{:s}:{:s}'.format(var,lev)
+        return '{:s}:{:s}'.format(var, lev)
 
     def range(self):
         '''range()
@@ -84,9 +86,9 @@ class IdxEntry(object):
         '''
         try:
             start = getattr(self, '_byte_start')
-            end   = getattr(self, '_byte_end')
-        except Exception as e:
-            raise Exception(e)
+            end = getattr(self, '_byte_end')
+        except Exception as ex:
+            raise Exception(ex)
         end = '' if end is None else '{:d}'.format(end)
 
         return '{:d}-{:s}'.format(start, end)
@@ -99,51 +101,60 @@ class IdxEntry(object):
         else:
             end = '{:d}'.format(self._byte_end)
         return 'IDX ENTRY: {:10d}-{:>10s}, \'{:s}\''.format(self._byte_start,
-                end, self.key())
+                                                            end, self.key())
 
 # Functions
+
+
 def get_file_names(data_path_gribfile, baseurl, date, mem, step, modeltype, resolution):
     '''With this function the file names from the server are preprocessed.'''
 
     if modeltype in ['avg', 'spr']:
         if resolution == 0.5:
             # Create URL for geavg.t00z.pgrb2a.0p50.f000.idx
-            filename = 'ge{:s}.t{:s}z.pgrb2a.0p50.f{:03d}'.format(modeltype, date.strftime('%H'), step)
+            filename = 'ge{:s}.t{:s}z.pgrb2a.0p50.f{:03d}'.format(
+                modeltype, date.strftime('%H'), step)
         elif resolution == 1:
             # since UPDATE of noa server depraced 2020-09-27
             # Create URL for geavg.t00z.pgrb2af00.idx
-            filename = 'ge{:s}.t{:s}z.pgrb2af{:s}'.format(modeltype, date.strftime('%H'), '{:02d}'.format(step) if step < 100 else '{:03d}'.format(step))
+            filename = 'ge{:s}.t{:s}z.pgrb2af{:s}'.format(modeltype, date.strftime(
+                '%H'), '{:02d}'.format(step) if step < 100 else '{:03d}'.format(step))
         else:
             logging.error('The resolution %d ist not supported', resolution)
             sys.exit(1)
 
         gribfile = os.path.join(date.strftime(baseurl), filename)
-        local = date.strftime('GFEE_%Y%m%d_%H00') + '_{:s}_f{:03d}.grb2'.format(modeltype, step)
-        subset = date.strftime('GFSE_%Y%m%d_%H00') + '_{:s}_f{:03d}_subset.grb2'.format(modeltype, step)
+        local = date.strftime('GFEE_%Y%m%d_%H00') + \
+            '_{:s}_f{:03d}.grb2'.format(modeltype, step)
+        subset = date.strftime('GFSE_%Y%m%d_%H00') + \
+            '_{:s}_f{:03d}_subset.grb2'.format(modeltype, step)
     else:
         # UPDATE NAMES 2020-09-27
         # Create URL for gec00.t00z.pgrb2a.0p50.f000.idx
         # Create URL for gep01.t00z.pgrb2a.0p50.f000.idx
         gribfile = os.path.join(date.strftime(baseurl),
-                            'ge{:s}{:02d}.t{:s}z.pgrb2a.0p50.f{:s}'.format(
-                            'c' if mem == 0 else 'p', mem, date.strftime('%H'),
-                            '{:03d}'.format(step)))
-        local    = date.strftime('GEFS_%Y%m%d_%H00') + '_{:02d}_f{:03d}.grb2'.format(mem, step)
-        subset   = date.strftime('GEFS_%Y%m%d_%H00') + '_{:02d}_f{:03d}_subset.grb2'.format(mem, step)
-    return {'grib'   : gribfile,
-            'idx'    : '{:s}.idx'.format(gribfile), 
-            'local'  : os.path.join(data_path_gribfile, local),
-            'subset' : os.path.join(data_path_gribfile, subset)}
+                                'ge{:s}{:02d}.t{:s}z.pgrb2a.0p50.f{:s}'.format(
+            'c' if mem == 0 else 'p', mem, date.strftime('%H'),
+            '{:03d}'.format(step)))
+        local = date.strftime('GEFS_%Y%m%d_%H00') + \
+            '_{:02d}_f{:03d}.grb2'.format(mem, step)
+        subset = date.strftime('GEFS_%Y%m%d_%H00') + \
+            '_{:02d}_f{:03d}_subset.grb2'.format(mem, step)
+    return {'grib': gribfile,
+            'idx': '{:s}.idx'.format(gribfile),
+            'local': os.path.join(data_path_gribfile, local),
+            'subset': os.path.join(data_path_gribfile, subset)}
 
 
 def parse_index_file(idxfile, params):
     '''A function for processing the GEFS idx files'''
     try:
         logging.info('The index file is going to be downloaded: %s', idxfile)
-        req  = requests.get(idxfile)
+        req = requests.get(idxfile)
         data = req.text.split('\n')
     except:
-        logging.error('Problems reading index file ... %s ... return None', idxfile)
+        logging.error(
+            'Problems reading index file ... %s ... return None', idxfile)
         return None
 
     # List to store the required index message information
@@ -151,12 +162,13 @@ def parse_index_file(idxfile, params):
 
     comp = re.compile(r'^\d+:(\d+):d=\d{10}:([^:.*]*):([^:.*]*)')
     for line in data:
-        if len(line) == 0: continue
-        mtch = re.findall(comp, line)
-        if not mtch:
+        if len(line) == 0:
+            continue
+        match = re.findall(comp, line)
+        if not match:
             raise Exception('whoops, pattern mismatch \'{:s}\''.format(line))
         # Else crate the variable hash
-        idx_entries.append(IdxEntry(mtch[0]))
+        idx_entries.append(IdxEntry(match[0]))
 
     for k in range(0, len(idx_entries)):
         if (k + 1) == len(idx_entries):
@@ -166,7 +178,8 @@ def parse_index_file(idxfile, params):
 
     res = []
     for x in idx_entries:
-        if x.key() in params: res.append(x.range())
+        if x.key() in params:
+            res.append(x.range())
 
     return res
 
@@ -182,12 +195,14 @@ def download_grib(grib, local, required):
             f.close()
         return True
     except:
-        logging.error('The Grbifile \'%s\' could not be loaded and saved in \'%s\'', grib, local)
+        logging.error(
+            'The Grbifile \'%s\' could not be loaded and saved in \'%s\'', grib, local)
         return False
+
 
 def fetch_gefs_data(modeltype, date, parameter, resolution):
     '''Function for downloading gribfiles from the GEFS NCEP server.'''
-    
+
     # A failure variable if an error occurs
     exit_with_error = False
 
@@ -207,13 +222,14 @@ def fetch_gefs_data(modeltype, date, parameter, resolution):
 
     # runhour is in [0, 6, 12, 18]
     runhour = 0
-    date = datetime.strptime('{:s} {:02d}:00:00'.format(date, runhour), '%Y%m%d %H:%M:%S')
+    date = datetime.strptime('{:s} {:02d}:00:00'.format(
+        date, runhour), '%Y%m%d %H:%M:%S')
 
     # Steps/members. The +1 is required to get the required sequence!
     steps = range(6, 300+1, 6)
 
     logging.info('{:s}'.format(''.join(['-']*70)))
-    
+
     # https://www.nco.ncep.noaa.gov/pmb/products/gens/
     if modeltype in ['avg', 'spr']:
         members = range(0, 1, 1)
@@ -232,18 +248,19 @@ def fetch_gefs_data(modeltype, date, parameter, resolution):
     elif modeltype in ['ens']:
         data_path = f'./data/get_available_data/gefs_ens_forecast_p05/{parameter}'
         members = range(0, 30+1, 1)
-        logging.info('Downloading members: {:s}'.format(', '.join(['{:d}'.format(x) for x in members])))
+        logging.info('Downloading members: {:s}'.format(
+            ', '.join(['{:d}'.format(x) for x in members])))
         # url exchanged on 2020-09-23 response with a 404 error
         #baseurl = ' http://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.%Y%m%d/%H/pgrb2/'
         baseurl = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.%Y%m%d/00/atmos/pgrb2ap5/'
     else:
         logging.info('The modeltype is not supported: %s', modeltype)
         sys.exit(1)
-   
 
     steps_str = ', '.join(['{:d}'.format(x) for x in steps])
     logging.info('Downloading steps: %s', steps_str)
-    logging.info('For date/model initialization %s', date.strftime('%Y-%m-%d %H:%M UTC'))
+    logging.info('For date/model initialization %s',
+                 date.strftime('%Y-%m-%d %H:%M UTC'))
     logging.info('Base url: %s', date.strftime(baseurl))
     logging.info('{:s}'.format(''.join(['-']*70)))
 
@@ -253,19 +270,24 @@ def fetch_gefs_data(modeltype, date, parameter, resolution):
         for step in steps:
             logging.info('{:s}'.format(''.join(['-']*70)))
             if modeltype in ['avg', 'spr']:
-                logging.info('Processing +{:03d}h forecast, {:s}'.format(step, modeltype))
+                logging.info(
+                    'Processing +{:03d}h forecast, {:s}'.format(step, modeltype))
             else:
-                logging.info('Processing +{:03d}h forecast, member {:02d}'.format(step, mem))
+                logging.info(
+                    'Processing +{:03d}h forecast, member {:02d}'.format(step, mem))
 
             # Specify and create output directory if necessary
-            data_path_gribfile = '{:s}/{:s}'.format(data_path, date.strftime('%Y%m%d%H%M'))
+            data_path_gribfile = '{:s}/{:s}'.format(
+                data_path, date.strftime('%Y%m%d%H%M'))
             if not os.path.isdir(data_path_gribfile):
                 try:
                     os.makedirs(data_path_gribfile)
                 except:
-                    raise Exception('Cannot create directory {:s}!'.format(data_path_gribfile))
+                    raise Exception(
+                        'Cannot create directory {:s}!'.format(data_path_gribfile))
 
-            files = get_file_names(data_path_gribfile, baseurl, date, mem, step, modeltype, resolution)
+            files = get_file_names(
+                data_path_gribfile, baseurl, date, mem, step, modeltype, resolution)
             if os.path.isfile(files['subset']):
                 logging.info('Local subset exists, skip: %s', files['subset'])
                 logging.info('{:s}'.format(''.join(['-']*70)))
@@ -284,11 +306,12 @@ def fetch_gefs_data(modeltype, date, parameter, resolution):
             required = parse_index_file(files['idx'], params)
 
             # If no messages found: continue
-            if required is None or len(required) == 0: 
+            if required is None or len(required) == 0:
                 exit_with_error = True
                 continue
 
-            download_grib_success = download_grib(files['grib'], files['local'], required)
+            download_grib_success = download_grib(
+                files['grib'], files['local'], required)
 
             if not download_grib_success:
                 exit_with_error = True
@@ -298,16 +321,19 @@ def fetch_gefs_data(modeltype, date, parameter, resolution):
             if not subset is None:
                 we_bounds = '{:.2f}:{:.2f}'.format(subset['W'], subset['E'])
                 sn_bounds = '{:.2f}:{:.2f}'.format(subset['S'], subset['N'])
-                cmd = ['wgrib2', files['local'], '-small_grib', we_bounds, sn_bounds, files['subset']]
+                cmd = ['wgrib2', files['local'], '-small_grib',
+                       we_bounds, sn_bounds, files['subset']]
                 logging.info('- Subsetting: {:s}'.format(' '.join(cmd)))
                 p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
-                out, err = p.communicate()
+                p.communicate()
 
                 if p.returncode == 0:
                     os.remove(files['local'])
-                    logging.info('Subset created, delete global file: %s', files['local'])
+                    logging.info(
+                        'Subset created, delete global file: %s', files['local'])
                 else:
-                    logging.error('Problem with subset, do not delete global grib2 file.')
+                    logging.error(
+                        'Problem with subset, do not delete global grib2 file.')
                     exit_with_error = True
             else:
                 logging.info('No subset was created')
