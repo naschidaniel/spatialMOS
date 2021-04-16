dd5b63031271b129fcf8b5f33e2d4ea9810580dc
Daniel Naschberger <daniel@naschi.at>
2021-04-16 19:52:31 +0200

    fix(spatial_util): typing

--- py_spatialmos/spatial_util.py
+++ py_spatialmos/spatial_util.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 '''spatial_util functions'''
 
+from typing import Dict, Union
 import spatial_rust_util
 
-def convert_measurements(measurements, columns):
+def convert_measurements(measurements: Dict[str, Dict[str, Union[str, float]]], columns: list[str]):
     '''convert_measurements wraps the spatial_rust_util.convert_measurements function'''
     return spatial_rust_util.convert_measurements(measurements, columns)
