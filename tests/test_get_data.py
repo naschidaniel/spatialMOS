fb35b5263705bd9c41bab8ffd464a23829b1bf9f
Daniel Naschberger <daniel@naschi.at>
2021-04-16 20:07:21 +0200

    chore(test_get_data): prettier

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""Unittest for the rust module suedtirol"""
+'''Unittest for the get_data modules'''
 
 import unittest
 from py_spatialmos import get_lwd_data
@@ -13,17 +13,17 @@ class TestExitCodes(unittest.TestCase):
 
     @staticmethod
     def test_fetch_lwd_data_ok():
-        """This test should complete successfully if all the data from lwd could be downloaded."""
+        '''This test should complete successfully if all the data from lwd could be downloaded.'''
         get_lwd_data.fetch_lwd_data()
 
     @staticmethod
     def test_fetch_suedtirol_data_ok():
-        """This test should complete successfully if all the data from suedtirol could be downloaded."""
+        '''This test should complete successfully if all the data from suedtirol could be downloaded.'''
         get_suedtirol_data.fetch_suedtirol_data('20210101', '20210102')
 
     @staticmethod
     def test_fetch_zamg_data_ok():
-        """This test should complete successfully if all the data from zamg could be downloaded."""
+        '''This test should complete successfully if all the data from zamg could be downloaded.'''
         get_zamg_data.fetch_zamg_data()
 
 if __name__ == '__main__':
