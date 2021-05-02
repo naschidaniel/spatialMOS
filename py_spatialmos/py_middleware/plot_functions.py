7e6c61f5ce290ab3563abcb34c83f320d9e19d85
Daniel Naschberger <daniel@naschi.at>
2021-05-02 19:54:24 +0200

    chore(prediction): run with run_script

--- py_spatialmos/py_middleware/plot_functions.py
+++ py_spatialmos/py_middleware/plot_functions.py
@@ -3,11 +3,9 @@
 """Help functions to create the spatialMOS plots,."""
 
 import os
-import sys
 import logging
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
-import numpy as np
 import datetime as dt
 from PIL import Image
 import cartopy.crs as ccrs
@@ -28,21 +26,18 @@ def resize_plot(path_filename, filepath, newwidth, ending):
         path_filename_resize = os.path.join(filepath, filename_resize)
         im.save(path_filename_resize, "JPEG")
         logging.info("The prediction '%s' plot with the new width of %d was created.", path_filename_resize, newwidth)
-    except IOError:
+    except IOError as ex:
         logging.error("The new file with the new width of %d from '%s' could not be created.", newwidth, path_filename)
-        sys.exit(1)
-        
+        raise ex
+
     plot_filenames = {}
     plot_filenames[f"path_filename_{ending}"] = path_filename_resize
     plot_filenames[f"filename_{ending}"] = filename_resize
     return plot_filenames
 
-def plot_forecast(parameter, xx, yy, plotparameter, gribfile_info, what):
+def plot_forecast(parameter, xx, yy, plotparameter, gribfiles_data, what):
     """A function to create the GEFS and spatialMOS forecast plots."""
-
-    anal_date = gribfile_info["anal_date_avg"]
-    valid_date = gribfile_info["valid_date_avg"]
-    step = gribfile_info["step"]
+    step = gribfiles_data["step"]
 
     fig_dpi=72
     fig = plt.figure(figsize=(1600/fig_dpi, 1600/fig_dpi), dpi=fig_dpi)
@@ -51,10 +46,10 @@ def plot_forecast(parameter, xx, yy, plotparameter, gribfile_info, what):
     im = ""
     if parameter == "tmp_2m" and what == "spatialmos_mean":
         im = plt.pcolormesh(xx, yy, plotparameter, cmap="RdBu_r", vmin=-40, vmax=40, shading="auto", transform=ccrs.PlateCarree())
-        plot_title="2m Temperatur spatialMOS MEAN [°C]"
+        plot_title="2m Temperatur MEAN [°C]"
     elif parameter == "tmp_2m" and what == "spatialmos_spread":
         im = plt.pcolormesh(xx, yy, plotparameter, cmap="Reds", vmin=0, vmax=5, shading="auto", transform=ccrs.PlateCarree())
-        plot_title="2m Temperatur spatialMOS SPREAD [°C]"
+        plot_title="2m Temperatur SPREAD [°C]"
     elif parameter == "tmp_2m" and what == "nwp_mean":
         im = plt.pcolormesh(xx, yy, plotparameter, cmap="RdBu_r", vmin=-40, vmax=40, shading="auto", transform=ccrs.PlateCarree())
         plot_title="2m Temperatur GEFS MEAN [°C]"
@@ -87,9 +82,11 @@ def plot_forecast(parameter, xx, yy, plotparameter, gribfile_info, what):
         plot_title="10m Windgeschwindigkeit GEFS SPREAD [km/h]"
 
     plt.title(plot_title, loc="center", fontsize=20)
-    plt.title(f"Gültig für {valid_date} | Step +{step}", loc="left", fontsize=15)
-    plt.title(f"GEFS Lauf {anal_date} UTC", loc="right", fontsize=15)
-    
+    anal_date_title = dt.datetime.strptime(gribfiles_data['anal_date'], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H')
+    valid_date_title = dt.datetime.strptime(gribfiles_data['valid_date'], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H')
+    plt.title(f"Gültig für {valid_date_title} UTC | Step +{step}", loc="left", fontsize=15)
+    plt.title(f"GEFS Lauf {anal_date_title} UTC", loc="right", fontsize=15)
+
     # Set Colorbar and Extend
     if what in ["nwp_mean", "nwp_spread"]:
         ax.set_extent([9.5, 17.5, 46, 49.5], ccrs.PlateCarree())
@@ -185,8 +182,8 @@ def plot_forecast(parameter, xx, yy, plotparameter, gribfile_info, what):
     gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='gray', alpha=0.4, linestyle='-')
     gl.top_labels = False
     gl.right_labels = False
-    gl.xlocator = mticker.FixedLocator(gribfile_info["longitude"])
-    gl.ylocator = mticker.FixedLocator(gribfile_info["latitude"])
+    gl.xlocator = mticker.FixedLocator(gribfiles_data["longitude"])
+    gl.ylocator = mticker.FixedLocator(gribfiles_data["latitude"])
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
 
@@ -194,14 +191,13 @@ def plot_forecast(parameter, xx, yy, plotparameter, gribfile_info, what):
     if not os.path.exists(filepath):
         os.makedirs(filepath)
 
-    anal_date_timestamp = dt.datetime.strptime(anal_date, "%Y-%m-%d %H:%M")
-    anal_date_str = anal_date_timestamp.strftime("%Y%m%d")
+    anal_date_str = dt.datetime.strptime(gribfiles_data['anal_date'], "%Y-%m-%d %H:%M:%S").strftime("%Y%m%d")
     filename = f"{what}_{anal_date_str}_step_{step:03d}.jpg"
     path_filename = os.path.join(filepath, filename)
     fig.savefig(path_filename, bbox_inches="tight", pil_kwargs={"quality": 70, "optimize": True, "progressive": True})
     plt.close(fig=None)
     logging.info("The prediction plot '%s' was created.", path_filename)
-    
+
     # Create a small Version of the plot
     plot_filenames_sm = resize_plot(path_filename, filepath, 640, "sm")
     plot_filenames_md = resize_plot(path_filename, filepath, 1024, "md")
