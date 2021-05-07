3b4a237b9fbc226be85554ab1db239eae77b7749
Daniel Naschberger <daniel@naschi.at>
2021-05-07 17:59:44 +0200

    chore(pre_processing_interpolate_gribfiles): init

---
+++ py_spatialmos/pre_processing_interpolate_gribfiles.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+''' With this Python script the downloaded gribfiles can be interpolated to station locations.'''
+
+def run_interpolate_gribfiles(parser_dict):
+    '''run_interpolate_gribfiles is used to start the interpolation to the stations locations'''
+    print(parser_dict)
