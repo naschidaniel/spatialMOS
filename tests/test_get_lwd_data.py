c2df191c9e87435a281be64395ab7271bc5fa54f
Daniel Naschberger <daniel@naschi.at>
2021-08-03 16:36:03 +0200

    chore(test_get_lwd_data): test only if necessary

--- tests/test_get_lwd_data.py
+++ tests/test_get_lwd_data.py
@@ -7,6 +7,7 @@ import csv
 import unittest
 import json
 import tempfile
+from datetime import datetime
 from pathlib import Path
 import shutil
 from py_spatialmos import get_lwd_data
@@ -16,10 +17,20 @@ class TestExitCodes(unittest.TestCase):
 
     def test_fetch_lwd_data_ok(self):
         '''This test should complete successfully if all the data from lwd could be downloaded.'''
+
+        current_minute = int(datetime.now().strftime("%M"))
+        if current_minute > 25:
+            print(f'The Test test_fetch_lwd_data_ok has been skiped because of the current minute {current_minute}')
+            return
+
         data_path = Path(tempfile.mkdtemp())
         ogd_path = Path(tempfile.mkdtemp())
-        get_lwd_data.fetch_lwd_data(data_path, ogd_path)
+        with self.assertRaises(RuntimeError):
+            get_lwd_data.fetch_lwd_data(data_path, ogd_path)
+            shutil.rmtree(ogd_path)
+            shutil.rmtree(data_path)
 
+        get_lwd_data.fetch_lwd_data(data_path, ogd_path)
         try:
             for csv_file in data_path.glob('*.csv'):
                 with open(csv_file) as f:
@@ -35,6 +46,20 @@ class TestExitCodes(unittest.TestCase):
                 self.assertEqual(['features', 'type'], list(ogd_data.keys()))
         finally:
             shutil.rmtree(ogd_path)
+
+    def test_fetch_lwd_data_fail(self):
+        '''This test should complete successfully if all the data from lwd could be downloaded.'''
+        current_minute = int(datetime.now().strftime("%M"))
+        if 25 <= current_minute < 35:
+            print(f'The Test test_fetch_lwd_data_fail has been skiped because of the current minute {current_minute}')
+            return
+
+        data_path = Path(tempfile.mkdtemp())
+        ogd_path = Path(tempfile.mkdtemp())
+        with self.assertRaises(RuntimeError):
+            get_lwd_data.fetch_lwd_data(data_path, ogd_path)
+            shutil.rmtree(ogd_path)
+            shutil.rmtree(data_path)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
