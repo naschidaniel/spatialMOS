c27628080f5820a4010ad4a17986cf3a482d56e4
Daniel Naschberger <daniel@naschi.at>
2021-08-08 18:48:21 +0200

    chore(website): inti webpage

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>moses.tirol</h1>
+    <p>This domain is only used for email redirect.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+});
+</script>
+
+<style scoped>
+h1 {
+  margin: 100px 0 0;
+  color: #d3d3d3;
+}
+p {
+  color: #d3d3d3;
+  font-size: 150%;
+}
+</style>
