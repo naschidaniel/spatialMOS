0552ea998fca7e07cabfe19565602d8bc4d93ba4
Daniel Naschberger <daniel@naschi.at>
2021-08-10 18:03:19 +0200

    chore(pages): refact pages

--- website/src/pages/Kontakt.vue
+++ website/src/pages/Kontakt.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="container-fluid">Kontakt</div>
+  <div class="container-fluid">
+    <h1>Kontakt</h1>
+    <strong>Daniel Naschberger</strong><br />
+    <p>
+      Peter-Mayr-Straße 29<br />
+      A-6020 Innsbruck
+    </p>
+    <p>
+      <a href="mailto:daniel@naschi.at">daniel@naschi.at</a><br />
+      <a href="tel:004369910030004">+43 699 10030004</a>
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
