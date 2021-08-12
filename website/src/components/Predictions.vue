895aa3d16d6d146423df0f5193fb12d9d120610f
Daniel Naschberger <daniel@naschi.at>
2021-08-12 20:55:34 +0200

    chore(Predictions): restore step Navigation

--- website/src/components/Predictions.vue
+++ website/src/components/Predictions.vue
@@ -1,28 +1,68 @@
 <template>
   <div>
-    <div>Analyse Datum: {{ predictions.analDate }}</div>
-    <img class="img-fluid pointer" :src="imgHref" @click="setStep(+1)" />
-    <div>
-      <span class="pointer" @click="setStep(-1)">back</span>
-      <span v-for="s in predictions.steps" :key="s">
-        <span
-          class="pointer"
-          :class="s === predictions.step ? 'text-danger' : ''"
-          @click="setStep(s)"
-          >{{ s }}</span
-        >
-      </span>
-      <span class="pointer" @click="setStep(+1)">next</span>
+    <div class="d-flex justify-content-center">
+      <img class="img-fluid pointer" :src="imgHref" @click="setStep(+1)" />
     </div>
+    <div class="mt-3 d-flex justify-content-between">
+      <button class="btn btn-light" type="button" @click="setStep(-1)">
+        <SolidChevronLeftIcon
+          class="text-body"
+          style="width: 1rem; height: 1rem"
+        />
+      </button>
+      <div class="d-none d-xl-block d-xxl-block my-2">
+        <span v-for="s in predictions.steps" :key="s">
+          <span
+            class="pointer m-1"
+            :class="s === predictions.step ? 'text-danger' : ''"
+            @click="setStep(s)"
+            >{{ s }}</span
+          >
+        </span>
+      </div>
+      <div class="d-block d-xl-none d-xxl-none my-2">
+        <span class="text-primary">Step: {{ predictions.step }}</span>
+      </div>
+      <button class="btn btn-light" type="button" @click="setStep(+1)">
+        <SolidChevronRightIcon
+          class="text-body"
+          style="width: 1rem; height: 1rem"
+        />
+      </button>
+    </div>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Wert</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Analyse Datum</th>
+          <td>{{ predictions.analDate }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Parameter</th>
+          <td>{{ predictions.parameter }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { usePrediction } from "../store/predictions";
+import SolidChevronLeftIcon from "./icons/SolidChevronLeftIcon.vue";
+import SolidChevronRightIcon from "./icons/SolidChevronRightIcon.vue";
 
 export default defineComponent({
   name: "Predictions",
+  components: {
+    SolidChevronLeftIcon,
+    SolidChevronRightIcon,
+  },
   setup() {
     const { predictions, fetchPrediction, selectedStep, setStep } =
       usePrediction();
