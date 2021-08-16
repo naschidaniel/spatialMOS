b580c02d8f2c759c03a3c9b89dd44c11765a01c0
Daniel Naschberger <daniel@naschi.at>
2021-08-16 20:53:08 +0200

    chore(Systemstatus): add systemstatus

---
+++ website/src/components/SystemChecks.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item">Check</li>
+    <li
+      v-for="systemCheck in systemstatus.systemChecks"
+      :key="systemCheck.name"
+      class="list-group-item"
+    >
+      <span class="d-inline d-sm-inline d-md-none ml-1">
+        <small>
+          <span
+            class="mr-1 badge rounded-pill mr-2"
+            :class="systemCheck.isTooOld ? 'bg-success' : 'bg-danger'"
+            title="{{ systemCheck.taskFinishedTime }}"
+            >&nbsp;</span
+          >
+          {{ systemCheck.taskname }}
+        </small>
+      </span>
+      <span class="d-none d-md-inline ml-2">
+        <span
+          class="d-none d-md-inline ml-2 mr-3 badge rounded-pill"
+          :class="systemCheck.isTooOld ? 'bg-success' : 'bg-danger'"
+          style="width: 60px"
+          :title="`${systemCheck.taskFinishedTime} ${systemCheck.taskname}`"
+        >
+          {{ systemCheck.isTooOld ? "passed" : "failed" }}
+        </span>
+        <span class="mx-2">{{ systemCheck.taskname }}</span
+        >&ndash;<small class="mx-2">{{
+          formatDateTime(systemCheck.taskFinishedTime)
+        }}</small>
+      </span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useSystemstatus } from "../store/systemstatus";
+import { formatDateTime } from "../util/formatters";
+
+export default defineComponent({
+  name: "SystemChecks",
+  setup() {
+    const { systemstatus } = useSystemstatus();
+    return { systemstatus };
+  },
+  methods: {
+    formatDateTime(date: string | undefined) {
+      return formatDateTime(Date.parse(date));
+    },
+  },
+});
+</script>
