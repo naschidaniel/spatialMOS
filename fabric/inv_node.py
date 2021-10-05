3a3c5be7ac5c03183aee98930b75cf1f17038a5c
Daniel Naschberger <daniel@naschi.at>
2021-10-05 21:17:38 +0200

    chore(docker): drop docker-compose

--- fabric/inv_node.py
+++ fabric/inv_node.py
@@ -2,18 +2,25 @@
 #  -*- coding: utf-8 -*-
 """The collection is needed for node."""
 
-import logging
 from invoke import task, Collection
-from . import inv_base
+from . import inv_docker
 from . import inv_logging
+
 
 @task
 def build(c):
     """This task is used to build the Javascript components"""
     inv_logging.task(build.__name__)
-    user, group = inv_base.uid_gid(c)
-    inv_base.docker_compose(c, f"run -u {user}:{group} node yarn build", pty=True)
-    logging.info("The Javascript components were built, minified and zipped.")
+    cmd = ["yarn", "build"]
+    inv_docker.run_node(c, cmd)
+    inv_logging.success(build.__name__)
+
+@task
+def dev(c):
+    """This task is used for development"""
+    inv_logging.task(build.__name__)
+    cmd = ["yarn", "dev"]
+    inv_docker.run_node(c, cmd)
     inv_logging.success(build.__name__)
 
 
@@ -21,8 +28,8 @@ def build(c):
 def lint(c):
     """This task is used to embellish the code"""
     inv_logging.task(lint.__name__)
-    user, group = inv_base.uid_gid(c)
-    inv_base.docker_compose(c, f"run -u {user}:{group} node yarn lint", pty=True)
+    cmd = ["yarn", "lint"]
+    inv_docker.run_node(c, cmd)
     inv_logging.success(lint.__name__)
 
 
@@ -30,13 +37,14 @@ def lint(c):
 def yarn(c, cmd):
     """This task is used to respond to the packet manager yarn, for example: yarn add date-fns"""
     inv_logging.task(yarn.__name__)
-    user, group = inv_base.uid_gid(c)
     inv_logging.cmd(cmd)
-    inv_base.docker_compose(c, f"run -u {user}:{group} node yarn {cmd}", pty=True)
+    command = ["yarn", cmd]
+    inv_docker.run_node(c, command)
     inv_logging.success(yarn.__name__)
 
 
 NODE_NS = Collection("node")
 NODE_NS.add_task(build)
+NODE_NS.add_task(dev)
 NODE_NS.add_task(lint)
 NODE_NS.add_task(yarn)
--- fabric/inv_spatialmos.py
+++ fabric/inv_spatialmos.py
@@ -18,13 +18,11 @@ from . import inv_docker
 def spatialmos__init_topography(c):
     """Create shapefiles for spatialMOS"""
     inv_logging.task(spatialmos__init_topography.__name__)
-    cmd = ["r_base", "Rscript", "./r_spatialmos/init_shapefiles.R"]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    cmd = ["Rscript", "./r_spatialmos/init_shapefiles.R"]
+    inv_docker.run_r_base(c, cmd)
 
-    cmd = ['py_container', 'python', './run_script.py', '--script', 'pre_processing_topography']
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    cmd = ['python', './run_script.py', '--script', 'pre_processing_topography']
+    inv_docker.run_py_container(c, cmd)
 
     # Download Shapefile and unzip it
     if not os.path.exists("./data/get_available_data/gadm/gadm36_AUT_shp"):
@@ -48,19 +46,18 @@ def spatialmos__init_topography(c):
 def py_spatialmos__archive_folder(c, folder):
     """The *.tar.gz are created with tar. The folder must be specified e.g. zamg."""
     inv_logging.task(py_spatialmos__archive_folder.__name__)
-    cmd = ['py_container', 'python', './run_script.py', '--script', 'archive_folder', '--folder', folder]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__archive_folder.__name__, cmd)
+    cmd = ['./run_script.py', '--script', 'archive_folder', '--folder', folder]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__archive_folder.__name__)
+
 
 @task
 def py_spatialmos__untar_folder(c, folder):
     """The *.tar.gz untared with tar. The fileprefix must be specified e.g. zamg."""
     inv_logging.task(py_spatialmos__untar_folder.__name__)
-    cmd = ['py_container', 'python', './run_script.py', '--script', 'untar_folder', '--folder', folder]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__untar_folder.__name__, cmd)
+    cmd = ['python', './run_script.py', '--script', 'untar_folder', '--folder', folder]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__untar_folder.__name__)
 
 
 @task
@@ -84,21 +81,21 @@ def py_spatialmos__merge_statusfiles(c):
     systemchecks_done = sorted([c["checkName"] for c in statusfiles])
     systemchecks_missing = [check for check in systemchecks_available if check not in systemchecks_done]
     if len(systemchecks_missing) == 0:
-        inv_base.write_statusfile_and_success_logging("py_spatialmos__available_systemchecks", "")
+        inv_base.write_statusfile_and_success_logging("py_spatialmos__available_systemchecks")
     else:
         for check in systemchecks_missing:
             logging.error("The check '%s' is missing", check)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__merge_statusfiles.__name__, "")
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__merge_statusfiles.__name__)
 
 
 @task
 def py_spatialmos__get_gefs(c, date, resolution, modeltype, parameter):
     """Download data gefs files."""
     inv_logging.task(py_spatialmos__get_gefs.__name__)
-    cmd = ["py_container", "python", "./run_script.py", "--script", "get_gefs_forecasts", "--date", date, "--resolution", resolution, "--modeltype", modeltype, "--parameter", parameter]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_gefs.__name__, cmd)
+    cmd = ["python", "./run_script.py", "--script", "get_gefs_forecasts", "--date", date, "--resolution", resolution, "--modeltype", modeltype, "--parameter", parameter]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_gefs.__name__)
+
 
 @task
 def py_spatialmos__get_gefs_forecasts(c, date, parameter):
@@ -123,94 +120,78 @@ def py_spatialmos__get_suedtirol(c, begindate, enddate):
     inv_logging.task(py_spatialmos__get_suedtirol.__name__)
     cmd = ["py_container", "python", "./run_script.py", "--script", "get_suedtirol_data",
            "--begindate", begindate, "--enddate", enddate]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_suedtirol.__name__, cmd)
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_suedtirol.__name__)
 
 
 @task
 def py_spatialmos__get_lwd(c):
     """Download data from lwd tirol"""
     inv_logging.task(py_spatialmos__get_lwd.__name__)
-    cmd = ["py_container", "python", "./run_script.py", "--script", "get_lwd_data"]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_lwd.__name__, cmd)
+    cmd = ["python", "./run_script.py", "--script", "get_lwd_data"]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_lwd.__name__)
 
 
 @task
 def py_spatialmos__get_zamg(c):
     """Download data from zamg webpage."""
     inv_logging.task(py_spatialmos__get_zamg.__name__)
-    cmd = ["py_container", "python", "./run_script.py", "--script", "get_zamg_data"]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_zamg.__name__, cmd)
+    cmd = ["python", "./run_script.py", "--script", "get_zamg_data"]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__get_zamg.__name__)
 
 
 @task
 def py_spatialmos__combine_data(c, folder):
     """Combine downloaded data for a folder."""
     inv_logging.task(py_spatialmos__combine_data.__name__)
-    cmd = ["py_container", "python", "./run_script.py", "--script", "combine_data", "--folder", folder]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    cmd = ["python", "./run_script.py", "--script", "combine_data", "--folder", folder]
+    inv_docker.run_py_container(c, cmd)
 
 @task
 def py_spatialmos__interpolate_gribfiles(c, parameter):
     """GEFS Reforecasts are bilinear interpolated at station locations."""
     inv_logging.task(py_spatialmos__interpolate_gribfiles.__name__)
-    cmd = ['py_container', 'python', './run_script.py', '--script', 'interpolate_gribfiles', '--parameter', parameter]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__interpolate_gribfiles.__name__, cmd)
-
-
-@task
-def py_spatialmos__pre_processing_observations_and_reforecasts_to_stations(c):
-    """Station Observations and GEFS Reforecasts are combined."""
-    inv_logging.task(py_spatialmos__pre_processing_observations_and_reforecasts_to_stations.__name__)
-    cmd = ["py_rdata", "python", "./py_spatialmos/pre_processing_station_observations_and_reforecasts.py"]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__pre_processing_observations_and_reforecasts_to_stations.__name__, cmd)
+    cmd = ['python', './run_script.py', '--script', 'interpolate_gribfiles', '--parameter', parameter]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__interpolate_gribfiles.__name__)
 
 
 @task
 def py_spatialmos__pre_processing_gamlss_crch_climatologies(c, parameter):
     """Create climatologies for further processing in R with gamlss."""
     inv_logging.task(py_spatialmos__pre_processing_gamlss_crch_climatologies.__name__)
-    cmd = ["py_rdata", "python", "./py_spatialmos/pre_processing_gamlss_crch_climatologies.py", "--parameter", parameter]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__pre_processing_gamlss_crch_climatologies.__name__, cmd)
+    cmd = ["python", "./py_spatialmos/pre_processing_gamlss_crch_climatologies.py", "--parameter", parameter]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__pre_processing_gamlss_crch_climatologies.__name__)
 
 
 @task
 def r_spatialmos__gamlss_crch_model(c, validation, parameter):
     """Create the required spatial climatologies."""
     inv_logging.task(r_spatialmos__gamlss_crch_model.__name__)
-    cmd = ["r_base", "Rscript", "./r_spatialmos/gamlss_crch_model.R", "--validation", validation, "--parameter", parameter]
+    cmd = ["Rscript", "./r_spatialmos/gamlss_crch_model.R", "--validation", validation, "--parameter", parameter]
     cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    inv_docker.run_r_base(c, cmd)
     inv_logging.success(r_spatialmos__gamlss_crch_model.__name__)
 
 @task
 def r_spatialmos__spatial_climatologies_nwp(c,  begin, end, parameter):
     """Create daily climatologies for the NWP."""
     inv_logging.task(r_spatialmos__spatial_climatologies_nwp.__name__)
-    cmd = ["r_base", "Rscript", "./r_spatialmos/spatial_climatologies_nwp.R",  "--begin", begin, "--end", end, "--parameter", parameter]
+    cmd = ["Rscript", "./r_spatialmos/spatial_climatologies_nwp.R",  "--begin", begin, "--end", end, "--parameter", parameter]
     cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    inv_docker.run_r_base(c, cmd)
     inv_logging.success(r_spatialmos__spatial_climatologies_nwp.__name__)
 
 @task
 def r_spatialmos__spatial_climatologies_obs(c, begin, end, parameter):
     """Create daily climatologies for the observations."""
     inv_logging.task(r_spatialmos__spatial_climatologies_obs.__name__)
-    cmd = ["r_base", "Rscript", "./r_spatialmos/spatial_climatologies_observations.R", "--begin", begin, "--end", end, "--parameter", parameter]
+    cmd = ["Rscript", "./r_spatialmos/spatial_climatologies_observations.R", "--begin", begin, "--end", end, "--parameter", parameter]
     cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
+    inv_docker.run_r_base(c, cmd)
     inv_logging.success(r_spatialmos__spatial_climatologies_obs.__name__)
 
 @task
@@ -218,48 +199,32 @@ def py_spatialmos__pre_processing_gribfiles(c, date, resolution, parameter):
     """Create the csv file and the jsonfile from the available gribfiles."""
     inv_logging.task(py_spatialmos__pre_processing_gribfiles.__name__)
     cmd = ["py_container", "python", "./run_script.py", "--script", "pre_processing_prediction", "--date", date, "--resolution", resolution, "--parameter", parameter]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__pre_processing_gribfiles.__name__, cmd)
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__pre_processing_gribfiles.__name__)
 
 @task
 def py_spatialmos__prediction(c, date, parameter):
     """Create the predictions and the spatialMOS plots."""
     inv_logging.task(py_spatialmos__prediction.__name__)
-    cmd = ["py_container", "python", "./run_script.py", "--script", "prediction", "--date", date, "--resolution", "0.5", "--parameter", parameter]
-    cmd = ' '.join(cmd)
-    inv_docker.run(c, cmd)
-    inv_base.write_statusfile_and_success_logging(py_spatialmos__prediction.__name__, cmd)
+    cmd = ["python", "./run_script.py", "--script", "prediction", "--date", date, "--resolution", "0.5", "--parameter", parameter]
+    inv_docker.run_py_container(c, cmd)
+    inv_base.write_statusfile_and_success_logging(py_spatialmos__prediction.__name__)
 
-
-@task
-def py_spatialmos__maturin_build(c):
-    """Build the Rust libraries for Spatialmos"""
-    inv_logging.task(py_spatialmos__maturin_build.__name__)
-    user, group = inv_base.uid_gid(c)
-    c.run("docker run --rm -v $(pwd):/io konstin2/maturin build --manylinux off")
-    # TODO rm sudo
-    c.run(f"sudo chown {user}:{group} -R target")
-    c.run("mv ./target/wheels/*.whl ./container/py_container/")
-    inv_logging.success(r_spatialmos__spatial_climatologies_obs.__name__)
-
-SPATIALMOS_DEVELOPMENT_NS = Collection("spatialmos")
-SPATIALMOS_DEVELOPMENT_NS.add_task(spatialmos__init_topography)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__archive_folder)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__untar_folder)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__get_gefs)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__get_gefs_forecasts)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__get_suedtirol)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__get_lwd)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__get_zamg)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__combine_data)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__interpolate_gribfiles)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__merge_statusfiles)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__pre_processing_observations_and_reforecasts_to_stations)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__pre_processing_gamlss_crch_climatologies)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__pre_processing_gribfiles)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__prediction)
-SPATIALMOS_DEVELOPMENT_NS.add_task(py_spatialmos__maturin_build)
-SPATIALMOS_DEVELOPMENT_NS.add_task(r_spatialmos__gamlss_crch_model)
-SPATIALMOS_DEVELOPMENT_NS.add_task(r_spatialmos__spatial_climatologies_nwp)
-SPATIALMOS_DEVELOPMENT_NS.add_task(r_spatialmos__spatial_climatologies_obs)
+SPATIALMOS_NS = Collection("spatialmos")
+SPATIALMOS_NS.add_task(spatialmos__init_topography)
+SPATIALMOS_NS.add_task(py_spatialmos__archive_folder)
+SPATIALMOS_NS.add_task(py_spatialmos__untar_folder)
+SPATIALMOS_NS.add_task(py_spatialmos__get_gefs)
+SPATIALMOS_NS.add_task(py_spatialmos__get_gefs_forecasts)
+SPATIALMOS_NS.add_task(py_spatialmos__get_suedtirol)
+SPATIALMOS_NS.add_task(py_spatialmos__get_lwd)
+SPATIALMOS_NS.add_task(py_spatialmos__get_zamg)
+SPATIALMOS_NS.add_task(py_spatialmos__combine_data)
+SPATIALMOS_NS.add_task(py_spatialmos__interpolate_gribfiles)
+SPATIALMOS_NS.add_task(py_spatialmos__merge_statusfiles)
+SPATIALMOS_NS.add_task(py_spatialmos__pre_processing_gamlss_crch_climatologies)
+SPATIALMOS_NS.add_task(py_spatialmos__pre_processing_gribfiles)
+SPATIALMOS_NS.add_task(py_spatialmos__prediction)
+SPATIALMOS_NS.add_task(r_spatialmos__gamlss_crch_model)
+SPATIALMOS_NS.add_task(r_spatialmos__spatial_climatologies_nwp)
+SPATIALMOS_NS.add_task(r_spatialmos__spatial_climatologies_obs)
