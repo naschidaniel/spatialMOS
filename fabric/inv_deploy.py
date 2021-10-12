f607757abbf7907a1557eaca4799db8c358ee6aa
Daniel Naschberger <daniel@naschi.at>
2021-10-12 16:15:01 +0200

    chore(inv_deploay): add ignore check_upstream

--- fabric/inv_deploy.py
+++ fabric/inv_deploy.py
@@ -13,24 +13,27 @@ from . import inv_rsync
 def check_upstream(c):
     """Check main"""
     inv_logging.task(check_upstream.__name__)
-    print("Do you really want to run on production? [y/N]")
+    print("Should the diff between origin/main and the current branch be checked? [yes/ignore/NO]")
     answer = input()
 
-    if answer.upper() not in ("Y", "YES", "JA", "J"):
+    if answer.upper() not in ("J", "JA", "I", "IGNORE", "Y", "YES"):
         sys.exit(1)
 
-    if c.run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip() != "main":
-        logging.error("You are not in the main branch. Only the main branch can be uploaded onto the server.")
-        sys.exit(1)
+    if answer.upper not in ("I", "IGNORE"):
+        logging.warning("The dff between origin/main and current branch is not checked.")
+    else:
+        if c.run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip() != "main":
+            logging.error("You are not in the main branch. Only the main branch can be uploaded onto the server.")
+            sys.exit(1)
 
-    c.run("git fetch origin main", hide=True)
-    if c.run("git diff origin/main", hide=True).stdout.strip() != "":
-        logging.error("Your local branch differs from upstream main (run git diff)")
-        sys.exit(1)
+        c.run("git fetch origin main", hide=True)
+        if c.run("git diff origin/main", hide=True).stdout.strip() != "":
+            logging.error("Your local branch differs from upstream main (run git diff)")
+            sys.exit(1)
 
-    if c.run("git status --short", hide=True).stdout.strip() != "":
-        logging.error("You have a dirty working directory (run git status)")
-        sys.exit(1)
+        if c.run("git status --short", hide=True).stdout.strip() != "":
+            logging.error("You have a dirty working directory (run git status)")
+            sys.exit(1)
     inv_logging.success(check_upstream.__name__)
 
 
