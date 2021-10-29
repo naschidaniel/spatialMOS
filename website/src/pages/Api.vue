a3e92a5ba80a0b0431182ec2df4ce18fcfb7dce1
Daniel Naschberger <daniel@naschi.at>
2021-10-29 19:38:29 +0200

    chore(API): update current json files

--- website/src/pages/Api.vue
+++ website/src/pages/Api.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="container">Api</div>
+  <div class="container-lg">
+    <h1>API</h1>
+    <p>
+      Die Karten und Vorhersagen können ohne Einschränkung verwendet werden.
+    </p>
+    <h2>Vorhersagekarten</h2>
+    <div class="list-group">
+      <li class="list-group-item list-group-item-action">
+        <a
+          href="https://moses.tirol/media/tmp_2m/spatialmosrun_tmp_2m.json"
+          rel="noopener"
+          target="_blank"
+          >/media/tmp_2m/spatialmosrun_tmp_2m.json</a
+        >
+      </li>
+      <li class="list-group-item list-group-item-action">
+        <a
+          href="https://moses.tirol/media/tmp_2m/spatialmosrun_rh_2m.json"
+          rel="noopener"
+          target="_blank"
+          >/media/tmp_2m/spatialmosrun_rh_2m.json</a
+        >
+      </li>
+    </div>
+    <h2 class="mt-4">Systemstatus</h2>
+    <div class="list-group">
+      <li class="list-group-item list-group-item-action">
+        <a
+          href="https://moses.tirol/media/systemstatus.json"
+          rel="noopener"
+          target="_blank"
+          >/media/systemstatus.json</a
+        >
+      </li>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
