b2adad6f8682c52ba2c16059f26a9d6668f6f205
Daniel Naschberger <daniel@naschi.at>
2021-12-29 21:30:04 +0100

    chore(router): add custon 404 page

---
+++ website/src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  created: function () {
+    window.location.href = "/404.html";
+  },
+});
+</script>
--- website/src/router/index.ts
+++ website/src/router/index.ts
@@ -9,6 +9,7 @@ import Projektbeschreibung from "../pages/Projektbeschreibung.vue";
 import Punktvorhersagen from "../pages/Punktvorhersagen.vue";
 import Systemstatus from "../pages/Systemstatus.vue";
 import Vorhersagekarten from "../pages/Vorhersagekarten.vue";
+import NotFound from "../pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,6 +23,7 @@ const router = createRouter({
     { path: "/projektbeschreibung", component: Projektbeschreibung },
     { path: "/punktvorhersagen", component: Punktvorhersagen },
     { path: "/systemstatus", component: Systemstatus },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
   ],
 });
 
